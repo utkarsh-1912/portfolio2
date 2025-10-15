11489fef67d4b77549f21738f2e15d48fd0d0fb6
Utkarsh Gupta <utkarsh.19.12.01@gmail.com>
2025-10-15 04:17:40 +0000

    replace this from footer 'The source code is on GitHub.' and rephase as

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -7,7 +7,7 @@ export function SiteFooter() {
     <footer className="py-6 md:px-8 md:py-0 border-t">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by{' '}
+          Built with <span className="text-primary">❤️</span> by{' '}
           <a
             href="https://github.com/utkarsh-1912"
             target="_blank"
@@ -15,15 +15,6 @@ export function SiteFooter() {
             className="font-medium underline underline-offset-4"
           >
             Utkarsh Gupta
-          </a>
-          . The source code is on{' '}
-          <a
-            href="https://github.com/utkarsh-1912/portfolio"
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            GitHub
           </a>
           .
         </p>
