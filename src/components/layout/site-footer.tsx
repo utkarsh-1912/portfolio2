abea5266075bc71e4c56a3c5a94977ce5f1e335d
Utkarsh Gupta <utkarsh.19.12.01@gmail.com>
2025-10-15 04:19:35 +0000

    no, replace with this '💙'

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -7,7 +7,7 @@ export function SiteFooter() {
     <footer className="py-6 md:px-8 md:py-0 border-t">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built with <span className="text-primary">❤️</span> by{' '}
+          Built with 💙 by{' '}
           <a
             href="https://github.com/utkarsh-1912"
             target="_blank"
