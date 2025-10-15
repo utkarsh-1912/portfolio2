465e07966c934013008e31b9cecfe85471394f67
Utkarsh Gupta <utkarsh.19.12.01@gmail.com>
2025-10-15 08:55:52 +0000

    fix container in navbar as default px-2 for mobile devices as padding la

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -29,7 +29,7 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-14 max-w-screen-2xl items-center px-4 md:px-8">
         <Link href="/" className="mr-6 flex items-center space-x-2">
           <Code2 className="h-6 w-6 text-primary" />
           <span className="font-bold font-headline">Utkristi</span>
@@ -57,7 +57,7 @@ export function SiteHeader() {
           <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
             <SheetTrigger asChild>
               <Button variant="ghost" size="icon" className="md:hidden">
-                <Menu />
+                <Menu className="h-6 w-6" />
                 <span className="sr-only">Toggle Menu</span>
               </Button>
             </SheetTrigger>
